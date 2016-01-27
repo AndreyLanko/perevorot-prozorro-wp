35ed2372c984448252fe5840b74fced26849a0df
LANKO <lanko@perevorot.com>
2016-01-27 13:58:03 +0200

    Изменение конфига по умолчанию wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -37,6 +37,10 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+define('WP_HOME', 'http://'.$_SERVER['SERVER_NAME']);
+define('WP_HOST', 'http://'.$_SERVER['SERVER_NAME']);
+define('WP_SITEURL', 'http://'.$_SERVER['SERVER_NAME'].'/pz-wp');
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
