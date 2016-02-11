97e4503282cb04105321f07952233415d7c12714
Andrey Kravchenko <php.code.god@gmail.com>
2016-02-11 16:22:05 +0200

    плагин соцсетей без обращения к апи при загрузке страницы

--- wp-content/plugins/lm-social-profile-widget/lm-social-profile-widget.php
+++ wp-content/plugins/lm-social-profile-widget/lm-social-profile-widget.php
@@ -21,6 +21,7 @@ class Lm_Social_Profile extends WP_Widget {
                 __('Social Networks Profiles', 'translation_domain'), // Name
                 array('description' => __('Links to social media profile', 'translation_domain'),)
         );
+
     }
 
     /**
@@ -152,6 +153,94 @@ class Lm_Social_Share extends WP_Widget {
                 __('Social Networks Shares', 'translation_domain'), // Name
                 array('description' => __('Links to share', 'translation_domain'),)
         );
+        $action = $_POST['actionSocial'];
+        if (method_exists($this, $action)) {
+            $post_data = $_POST;
+            $this->$action($post_data);
+        }
+    }
+
+    function updateCount($data) {
+      global $wpdb;
+      $table_name = "wp_lm_social";
+      $sql = "CREATE TABLE IF NOT EXISTS `".$table_name."` (
+  `id` int(11) NOT NULL,
+  `fb` int(11) NOT NULL DEFAULT '0',
+  `tw` int(11) NOT NULL DEFAULT '0',
+  `in` int(11) NOT NULL DEFAULT '0',
+  `vk` int(11) NOT NULL DEFAULT '0',
+  `gp` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+      $wpdb->query($sql);
+      $json_response = array(
+        'success' =>  false
+      );
+      $sql = "select count(*) from $table_name where id = ".$data['post_id'];
+      $result = $wpdb->get_var($wpdb->prepare($sql));
+      if ($result == 0) {
+        $insert_data = array(
+          'id'  =>  $data['post_id']
+        );
+        $wpdb->insert($table_name,$insert_data);
+      }
+      $function_name = $data['type']."_share_count";
+      if (method_exists($this,$function_name)) {
+        $count = $this->$function_name($data['current_url']);
+        $update_data = array(
+          $data['type'] =>  $count
+        );
+        $wpdb->update($table_name,$update_data,array('id'=>$data['post_id']));
+        $json_response['success'] = true;
+      }
+      echo json_encode($json_response);
+      exit();
+    }
+
+    function fb_share_count($url) {
+        $api = file_get_contents("http://api.facebook.com/method/links.getStats?urls=".$url."&format=json");
+        $count = reset(json_decode( $api ));
+        return $count->share_count;
+    }
+    function tw_share_count($url) {
+       // $api = file_get_contents( 'https://cdn.api.twitter.com/1/urls/count.json?url=' . $url );
+        $api = file_get_contents( 'http://opensharecount.com/count.json?url=' . $url );
+        $count = json_decode($api);
+        return $count->count;
+    }
+    function in_share_count($url) {
+        $api = file_get_contents('http://www.linkedin.com/countserv/count/share?format=json&url='. $url );
+        $count = json_decode($api);
+        return $count->count;
+    }
+    function vk_share_count($url) {
+        $api = file_get_contents('http://vkontakte.ru/share.php?act=count&index=1&url='. $url );
+        $tmp = array();
+        preg_match('/^VK.Share.count\(1, (\d+)\);$/i',$api,$tmp);
+        return $tmp[1];
+    }
+    function gp_share_count($url) {
+        $curl = curl_init();
+        curl_setopt( $curl, CURLOPT_URL, "https://clients6.google.com/rpc" );
+        curl_setopt( $curl, CURLOPT_POST, 1 );
+        curl_setopt( $curl, CURLOPT_POSTFIELDS, '[{"method":"pos.plusones.get","id":"p","params":{"nolog":true,"id":"' . $url . '","source":"widget","userId":"@viewer","groupId":"@self"},"jsonrpc":"2.0","key":"p","apiVersion":"v1"}]' );
+        curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $curl, CURLOPT_HTTPHEADER, array( 'Content-type: application/json' ) );
+        $curl_results = curl_exec( $curl );
+        curl_close( $curl );
+        $json = json_decode( $curl_results, true );
+        return intval( $json[0]['result']['metadata']['globalCounts']['count'] );
+    }
+
+    public function getPostData($id) {
+      global $wpdb;
+      $table_name = "wp_lm_social";
+      return $wpdb->get_row("SELECT * FROM $table_name WHERE id = $id");
+    }
+
+    public function getPostSocial($id,$type) {
+      $post = $this->getPostData($id);
+      if (!empty($post)) return $post->$type;
+      return 0;
     }
 
     /**
@@ -164,7 +253,7 @@ class Lm_Social_Share extends WP_Widget {
      */
     public function widget($args, $instance) {
         $datatitle = get_the_title();
-
+        $dataitem = get_the_ID();
         if (!is_singular('faq') & !is_post_type_archive('faq')){
             $datahref = get_the_permalink();           
         } else $datahref = $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
@@ -180,23 +269,24 @@ class Lm_Social_Share extends WP_Widget {
         $social_wrapper['after'] = '</li>';
 
         // social profile link
-        $facebook_profile = '<a data-type="fb" data-title="'.$datatitle.'" data-href="'.$datahref.'" href="" target="_blank"><i class="sprite-social-fb"></i></a>';
-        $google_profile = '<a data-type="gp" data-title="'.$datatitle.'" data-href="'.$datahref.'" href="" target="_blank"><i class="sprite-social-g"></i></a>';
-        $twitter_profile = '<a data-type="tw" data-title="'.$datatitle.'" data-href="'.$datahref.'" href="" target="_blank"><i class="sprite-social-tw"></i></a>';
-        $linkedin_profile = '<a data-type="in" data-title="'.$datatitle.'" data-href="'.$datahref.'" href="" target="_blank"><i class="sprite-social-in"></i></a>';
-        $vkontakte_profile = '<a data-type="vk" data-title="'.$datatitle.'" data-href="'.$datahref.'" href="" target="_blank"><i class="sprite-social-vk"></i></a>';
+        $facebook_profile = '<a data-item="'.$dataitem.'" data-type="fb" data-title="'.$datatitle.'" data-href="'.$datahref.'" href="" target="_blank"><i class="sprite-social-fb"></i></a>';
+        $google_profile = '<a data-item="'.$dataitem.'" data-type="gp" data-title="'.$datatitle.'" data-href="'.$datahref.'" href="" target="_blank"><i class="sprite-social-g"></i></a>';
+        $twitter_profile = '<a data-item="'.$dataitem.'" data-type="tw" data-title="'.$datatitle.'" data-href="'.$datahref.'" href="" target="_blank"><i class="sprite-social-tw"></i></a>';
+        $linkedin_profile = '<a data-item="'.$dataitem.'" data-type="in" data-title="'.$datatitle.'" data-href="'.$datahref.'" href="" target="_blank"><i class="sprite-social-in"></i></a>';
+        $vkontakte_profile = '<a data-item="'.$dataitem.'" data-type="vk" data-title="'.$datatitle.'" data-href="'.$datahref.'" href="" target="_blank"><i class="sprite-social-vk"></i></a>';
+        $post_id = get_the_ID();
 
-        $_fb = facebook_like_share_count($datahref);
-        $_gp = google_plusones($datahref);
-        $_ln = linkenin_share_count($datahref);
-        $_vk = vkontakte_share_count($datahref);
+        $_fb = $this->getPostSocial($post_id,'fb');
+        $_tw = $this->getPostSocial($post_id,'tw');
+        $_gp = $this->getPostSocial($post_id,'gp');
+        $_in = $this->getPostSocial($post_id,'in');
+        $_vk = $this->getPostSocial($post_id,'vk');
 
-        $facebook_count = ($_fb)?'<span class="share-count">'.facebook_like_share_count($datahref).'</span>':'';
-        $google_count = ($_gb)?'<span class="share-count">'.google_plusones($datahref).'</span>':'';
-      //  $twitter_count = '<span class="share-count">'.twitter_tweet_count($datahref).'</span>';
-        $twitter_count = '';
-        $linkedin_count = ($_ln)?'<span class="share-count">'.linkenin_share_count($datahref).'</span>':'';
-        $vkontakte_count = ($_vk)?'<span class="share-count">'.vkontakte_share_count($datahref).'</span>':'';
+        $facebook_count = ($_fb)?'<span class="share-count">'.$_fb.'</span>':'';
+        $google_count = ($_gp)?'<span class="share-count">'.$_gp.'</span>':'';
+        $twitter_count = ($_tw)?'<span class="share-count">'.$_tw.'</span>':'';
+        $linkedin_count = ($_in)?'<span class="share-count">'.$_in.'</span>':'';
+        $vkontakte_count = ($_vk)?'<span class="share-count">'.$_vk.'</span>':'';
 
         echo $args['before_widget'];        
 
@@ -296,38 +386,5 @@ function Lm_social_profile_widget_css() {
 }
 add_action('wp_enqueue_scripts', 'Lm_social_profile_widget_css');
 
-function facebook_like_share_count($url) {
-    $api = file_get_contents( 'http://graph.facebook.com/?id=' . $url );
-    $count = json_decode( $api );
-    return $count->shares;
-}
-function twitter_tweet_count($url) {
-   // $api = file_get_contents( 'https://cdn.api.twitter.com/1/urls/count.json?url=' . $url );
-    $api = file_get_contents( 'http://opensharecount.com/count.json?url=' . $url );
-    $count = json_decode($api);
-    return $count->count;
-}
-function linkenin_share_count($url) {
-    $api = file_get_contents('http://www.linkedin.com/countserv/count/share?format=json&url='. $url );
-    $count = json_decode($api);
-    return $count->count;
-}
-function vkontakte_share_count($url) {
-    $api = file_get_contents('http://vkontakte.ru/share.php?act=count&index=1&url='. $url );
-    $tmp = array();
-    preg_match('/^VK.Share.count\(1, (\d+)\);$/i',$api,$tmp);
-    return $tmp[1];
-}
-function google_plusones($url) {
-    $curl = curl_init();
-    curl_setopt( $curl, CURLOPT_URL, "https://clients6.google.com/rpc" );
-    curl_setopt( $curl, CURLOPT_POST, 1 );
-    curl_setopt( $curl, CURLOPT_POSTFIELDS, '[{"method":"pos.plusones.get","id":"p","params":{"nolog":true,"id":"' . $url . '","source":"widget","userId":"@viewer","groupId":"@self"},"jsonrpc":"2.0","key":"p","apiVersion":"v1"}]' );
-    curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
-    curl_setopt( $curl, CURLOPT_HTTPHEADER, array( 'Content-type: application/json' ) );
-    $curl_results = curl_exec( $curl );
-    curl_close( $curl );
-    $json = json_decode( $curl_results, true );
-    return intval( $json[0]['result']['metadata']['globalCounts']['count'] );
-};
+
 
