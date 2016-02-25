f72d75155b8745c65af973624692fa5a796b6780
Andrey Kravchenko <php.code.god@gmail.com>
2016-02-25 14:25:01 +0200

    добавили скрипт для отображени FB page feed

--- wp-content/themes/Prozzoro/author.php
+++ wp-content/themes/Prozzoro/author.php
@@ -1,4 +1,12 @@
 <?php get_header();  ?>
+	<div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v2.5";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
 <div class="container">
 	<?php  
 	$curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
