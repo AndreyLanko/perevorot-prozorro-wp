a12ac3b9228e7274506b5e176f24d75eec40fa22
Andrey Kravchenko <php.code.god@gmail.com>
2016-07-29 13:43:06 +0300

    Добавили в header код GTA

--- wp-content/themes/Prozzoro/header.php
+++ wp-content/themes/Prozzoro/header.php
@@ -27,16 +27,15 @@
     <link rel="image_src" href="<?php bloginfo('wpurl'); ?>/wp-content/themes/Prozzoro/images/logo-prozorro-big.png" />
 </head>
 <body <?php body_class(); ?>>
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-73774371-1', 'auto');
-      ga('send', 'pageview');
-
-    </script>
+    <!-- Google Tag Manager -->
+    <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-5WP37T"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5WP37T');</script>
+    <!-- End Google Tag Manager -->
 	<nav class="navbar navbar-default top-menu">
             <div class="container">
                  <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 no-padding ofnews">
